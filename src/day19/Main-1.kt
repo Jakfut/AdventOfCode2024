003e72c14ecd2047d385935ab7d8299ca7415cea
Jakfut <jakfut0@gmail.com>
2024-12-19 14:38:35 +0100

    day19 part1

---
+++ src/day19/Main-1.kt
@@ -0,0 +1,51 @@
+package day19
+
+import java.io.File
+import java.io.InputStream
+
+fun main(){
+    val inputStream: InputStream = File("src/day19/in").inputStream()
+    val lineList = mutableListOf<String>()
+
+    inputStream.bufferedReader().forEachLine { lineList.add(it) }
+
+    val towels:MutableList<String> = mutableListOf()
+    val targets:MutableList<String> = mutableListOf()
+    var isTowel = true
+    var score = 0
+
+    lineList.forEach {
+        if (it == "")
+            isTowel = false
+        else if (isTowel)
+            towels.addAll(it.split(",").toMutableList()).also { towels.replaceAll { it.replace(" ", "") } }
+        else
+            targets.add(it)
+    }
+
+    towels.sortByDescending { it.length }
+    //println(towels)
+    //println(targets)
+
+    targets.forEach {
+        if (findSolution(towels, "", it) != "")
+            score++
+    }
+
+    println(score)
+}
+
+fun findSolution(towels:MutableList<String>, curString:String,  target: String) : String {
+    if (curString == target)
+        return curString
+
+    towels.forEach {
+        if (target.startsWith(curString + it)) {
+            val result = findSolution(towels, curString + it, target)
+            if (result != "")
+                return result
+        }
+    }
+
+    return ""
+}
