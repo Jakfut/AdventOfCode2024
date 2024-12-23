c428c56df104afc4819ab4364e7ab3f1e0c941d7
Jakfut <jakfut0@gmail.com>
2024-12-23 15:06:06 +0100

    day23

--- src/day22/Main-2.kt
+++ src/day22/Main-2.kt
@@ -4,10 +4,43 @@ import java.io.File
 import java.io.InputStream
 
 fun main(){
-    val inputStream: InputStream = File("src/day19/example").inputStream()
+    val inputStream: InputStream = File("src/day22/in").inputStream()
     val lineList = mutableListOf<String>()
 
     inputStream.bufferedReader().forEachLine { lineList.add(it) }
 
+    val secretNumbers = lineList.map { it.toLong() }
+    val sequenceMap:MutableMap<String, Long> = mutableMapOf()
 
+
+    secretNumbers.forEach {
+        val currentSequenceMap:MutableMap<String, Long> = mutableMapOf()
+        var secretNumber = it
+        var lastSecretNumber = it
+        val currentSequence:MutableList<Long> = mutableListOf()
+
+        for (i in 1..2000) {
+            secretNumber = nextSecretNumber(secretNumber)
+
+            currentSequence.add(secretNumber % 10 - lastSecretNumber % 10)
+
+            if (currentSequence.size == 5){
+                val sequence = currentSequence.subList(0, 4).joinToString("")
+                if (!currentSequenceMap.containsKey(sequence))
+                    currentSequenceMap[sequence] = lastSecretNumber % 10
+                    currentSequence.removeAt(0)
+            }
+            lastSecretNumber = secretNumber
+        }
+
+        currentSequenceMap.forEach {
+            if (sequenceMap.containsKey(it.key)){
+                sequenceMap[it.key] = sequenceMap[it.key]!! + it.value
+            } else {
+                sequenceMap[it.key] = it.value
+            }
+        }
+    }
+
+    println(sequenceMap.maxOf { it.value })
 }
